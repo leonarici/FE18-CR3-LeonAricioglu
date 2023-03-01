a4d4cd8768500cd0490d65733b75630f127dc28f
Leon Arici <leonarici@icloud.com>
2023-03-01 10:40:47 +0100

    error resolved

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -12,24 +12,18 @@ import { IProducts } from '../products';
 
 export class ProductListComponent implements OnInit{
   products:IProducts[] = products;
-  product: IProducts= {} as IProducts;
-  id: number = 0;
-
+ 
   constructor(
-    private route: ActivatedRoute,
     private cartService: CartService
     ) {}
 
     addToCart(product:IProducts){
       window.alert('Your product has been added to the cart!');
-      this.cartService.addToCart(this.product)
+      this.cartService.addToCart(product)
     }
 
   ngOnInit(): void {
-    this.route.params.subscribe((params: Params)=> {
-      this.id = +params['productId'];
-      this.product= products[this.id];
-    })
+    
     
   }
 
